aacb38456615c9f7e6b7676f165152f7ded72580
tareeqadnaan806 <tareeqadnaan806@gmail.com>
2023-08-30 01:09:51 +0530

    The Fun Piano

---
+++ script.js
@@ -0,0 +1,115 @@
+window.addEventListener("keydown", (event)=>{
+    switch (event.key) {
+        case '2':
+            document.getElementById("sound1").play()
+            break;
+        case '1':
+            document.getElementById("sound2").play()
+            break;
+        case 'a':
+        case 'A':
+            document.getElementById("sound3").play()
+            break;
+        case '3':
+            document.getElementById("sound1").play()
+            break;
+        case '4':
+            document.getElementById("sound5").play()
+            break;
+        case '5':
+            document.getElementById("sound6").play()
+            break;
+        case '6':
+            document.getElementById("sound7").play()
+            break;
+        case '7':
+            document.getElementById("sound8").play()
+            break;
+        case '8':
+            document.getElementById("sound9").play()
+            break;
+        case '9':
+            document.getElementById("sound10").play()
+            break;
+        case '0':
+            document.getElementById("sound11").play()
+            break;
+        case 'w':
+        case 'W':
+            document.getElementById("sound12").play()
+            break;
+        case 'R':
+        case 'r':
+            document.getElementById("sound13").play()
+            break;
+        case 'z':
+        case 'Z':
+            document.getElementById("sound14").play()
+            break;
+        case 't':
+        case 'T':
+            document.getElementById("sound15").play()
+            break;
+        case 'u':
+        case 'U':
+            document.getElementById("sound16").play()
+            break;
+        case 'o':
+        case 'O':
+            document.getElementById("sound17").play()
+            break;
+        case 'C':
+        case 'c':
+            document.getElementById("sound18").play()
+            break;
+        case 'b':
+        case 'B':
+            document.getElementById("sound19").play()
+            break;
+        case 's':
+        case 'S':
+            document.getElementById("sound20").play()
+            break;
+        case 'f':
+        case 'F':
+            document.getElementById("sound21").play()
+            break;
+        case 'g':
+        case 'G':
+            document.getElementById("sound22").play()
+            break;
+        case 'l':
+        case 'L':
+            document.getElementById("sound23").play()
+            break;
+        case 'Q':
+        case 'q':
+            document.getElementById("sound24").play()
+            break;
+        case 'e':
+        case 'E':
+            document.getElementById("sound25").play()
+            break;
+        case 'y':
+        case 'Y':
+            document.getElementById("sound26").play()
+            break;
+        case 'i':
+        case 'I':
+            document.getElementById("sound27").play()
+            break;
+        case 'p':
+        case 'P':
+            document.getElementById("sound28").play()
+            break;
+        case 'v':
+        case 'V':
+            document.getElementById("sound29").play()
+            break;
+        case 'x':
+        case 'X':
+            document.getElementById("sound30").play()
+            break;
+    }
+    
+})
